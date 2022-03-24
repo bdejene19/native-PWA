ebb6e1175efc53aded42c8a4fb1b4725bf22ff2f
Bemnet Dejene <bdejene19@gmail.com>
2022-03-24 18:59:41 -0400

    trying to fix heroku deploy error

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -4,9 +4,8 @@ const butInstall = document.getElementById("buttonInstall");
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener("beforeinstallprompt", (event) => {
   // store events triggered for install
-
+  window.deferredPrompt = event;
   butInstall.classList.toggle("hidden", false);
-  return (window.deferredPrompt = event);
 });
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener("click", async () => {
